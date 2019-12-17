eaad12ccc944b2033a3de187275340d1ae71152a
Roman <r.gutsu@bk.ru>
2019-12-17 14:29:05 +0200

    eventDelegation

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-document.getElementById('cta2').onclick = function(e){
-    document.getElementById('area').innerHTML = "You press green button"
-};
 
-function addInnerHtml(e){
-    document.getElementById('area').innerHTML = "You press red button"
-}
+var area = document.getElementById('area');
 
-document.getElementById('cta3').addEventListener('click', addInnerHtml);
+document.querySelector('.buttons').addEventListener('click', clickButton);
+
+function clickButton(e) { 
+    if (e.target.classList.contains('bg-primary')) {
+      area.innerHTML = 'You clicked blue button';
+      console.log('capturing', this.tagName);
+    } else if(e.target.classList.contains('bg-success')){
+        area.innerHTML = 'You clicked green button';
+        console.log('capturing', this.tagName);
+    } else if(e.target.classList.contains('bg-danger')){
+        area.innerHTML = 'You clicked red button';
+        console.log('capturing' , this.tagName);
+    }
+  }
